08c5cc091bf174689f5d79bda3e72e68b5cdb059
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-05 14:19:11 +0800

    Add inefficient code for ex8

--- exercises/arrays-and-strings/ex8.js
+++ exercises/arrays-and-strings/ex8.js
@@ -3,7 +3,36 @@
 * row and column are set to 0.
 */
 
-// O(N) ?
-const bomberMan = () => {
+// O(MN) ?
+const bomberMan = (matrix) => {
+  var iCache = {};
+  var jCache = {};
   
+  for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < matrix[i].length; j++) {
+      let current = matrix[i][j];
+      
+      if (current === 0 && !(iCache[i] || jCache[j])) {
+        iCache[i] = true;
+        jCache[j] = true;
+        
+        for (var x = 0; x < matrix.length; x++) {
+          matrix[x][j] = 0;
+        }
+        
+        for (var y = 0; y < matrix[i].length; y++) {
+          matrix[i][y] = 0;
+        }
+      }
+    }
+  }
+  
+  return matrix;
 };
+
+console.log(bomberMan([
+  [1,1,1,1],
+  [1,0,1,1],
+  [1,1,1,1],
+  [1,1,1,1],
+]));

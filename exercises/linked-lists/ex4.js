52d5e3f814f78fe16453d3e94c44bb313be201d2
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-08 08:26:42 +0800

    Add initial solution for ex4

--- exercises/linked-lists/ex4.js
+++ exercises/linked-lists/ex4.js
@@ -10,6 +10,44 @@ const Node = require('./helpers').Node;
 const nodeLogger = require('./helpers').nodeLogger;
 const isNotNull = require('./helpers').nodeLogger;
 
-const partition = () => {
+const partition = (node, val) => {
+  var left, firstLeft, right, firstRight;
   
+  while (node !== null) {
+    let current = new Node(node.data);
+    
+    if (current.data < val) {
+      if (!left) {
+        left = current;
+        firstLeft = left;
+      } else {
+        left.next = current;
+        left = current;
+      }
+    } else {
+      if (!right) {
+        right = current;
+        firstRight = right;
+      } else {
+        right.next = current;
+        right = current;
+      }
+    }
+    
+    node = node.next;
+  }
+  
+  left.next = firstRight;
+  
+  return firstLeft;
 };
+
+
+let head = new Node(10);
+for (let i = 9; i > 0; i--) {
+  head.appendToTail(i);
+}
+
+console.log(nodeLogger(head));
+let newHead = partition(head, 5)
+console.log(nodeLogger(newHead));

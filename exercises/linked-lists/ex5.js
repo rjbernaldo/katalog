1c339b1fd21ee399a98a5c5a1a7f470dfd41fd5f
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-08 20:04:43 +0800

    Add WIP ex5

--- exercises/linked-lists/ex5.js
+++ exercises/linked-lists/ex5.js
@@ -1,7 +1,7 @@
 /*
 * You have two numbers represented by a linked list, where each node contains
 * a single digit. The digits are stored in reverse order, such that the 1's
-* digit is at thea head of the list. Write a function that adds the two
+* digit is at the head of the list. Write a function that adds the two
 * numbers and returns the sum as a linked list.
 */
 
@@ -9,6 +9,21 @@ const Node = require('./helpers').Node;
 const nodeLogger = require('./helpers').nodeLogger;
 const isNotNull = require('./helpers').nodeLogger;
 
-const sumLists = () => {
+const sumLists = (node1, node2) => {
+  if (node1 === null) return 0;
+  
   
 };
+
+// 101
+let node1 = new Node(1);
+node1.appendToTail(0);
+node1.appendToTail(1);
+
+// 50
+let node2 = new Node(5);
+node2.appendToTail(0);
+
+console.log(nodeLogger(node1));
+console.log(nodeLogger(node2));
+console.log(nodeLogger(sumLists(node1, node2)));

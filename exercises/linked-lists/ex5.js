5a267116f1c72cde3b47483f4fd9ba20487207f8
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-09 08:19:34 +0800

    Add WIP ex5

--- exercises/linked-lists/ex5.js
+++ exercises/linked-lists/ex5.js
@@ -9,21 +9,47 @@ const Node = require('./helpers').Node;
 const nodeLogger = require('./helpers').nodeLogger;
 const isNotNull = require('./helpers').isNotNull;
 
-const sumLists = (node1, node2) => {
-  if (node1 === null) return 0;
+const sumLists = (n1, n2) => {
+  return sumListsRecursively(n1, n2).sum;
+};
+
+const sumListsRecursively = (n1, n2) => {
+  // TODO: Padding
+  if (n1 === null && n2 === null) return { carry: 0 };
   
+  let next1 = n1 ? n1.next : null;
+  let next2 = n2 ? n2.next : null;
   
+  let current = sumListsRecursively(next1, next2);
+  
+  let n1Data = n1 ? n1.data : 0;
+  let n2Data = n2 ? n2.data : 0;
+  let val = n1Data + n2Data + current.carry;
+  
+  var carry;
+  if (val.toString().length > 1) {
+    carry = parseInt(val.toString()[0]);
+    val = parseInt(val.toString()[1]);
+  }
+  
+  let sum = new Node(val)
+  sum.next = current.sum;
+  current.sum = sum;
+  current.carry = carry || 0;
+  
+  return current;
 };
 
 // 101
-let node1 = new Node(1);
-node1.appendToTail(0);
-node1.appendToTail(1);
+let n1 = new Node(1);
+n1.appendToTail(5);
+n1.appendToTail(5);
 
 // 50
-let node2 = new Node(5);
-node2.appendToTail(0);
+let n2 = new Node(5);
+n2.appendToTail(5);
+n2.appendToTail(5);
 
-console.log(nodeLogger(node1));
-console.log(nodeLogger(node2));
-console.log(nodeLogger(sumLists(node1, node2)));
+// console.log(nodeLogger(n1));
+// console.log(nodeLogger(n2));
+console.log(nodeLogger(sumLists(n1, n2)));

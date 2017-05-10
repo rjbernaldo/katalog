5d2122bc045c18859c61f0f7726afbc036c33d7f
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-10 12:57:26 +0800

    Add another rough solution for ex8

--- exercises/linked-lists/ex8.js
+++ exercises/linked-lists/ex8.js
@@ -7,6 +7,21 @@ const Node = require('./helpers').Node;
 const nodeLogger = require('./helpers').nodeLogger;
 const isNotNull = require('./helpers').isNotNull;
 
-const loopDetection = () => {
+const loopDetection = (head, cache = []) => {
+  if (cache.indexOf(head) > 0) return head;
   
+  cache.push(head);
+  
+  return loopDetection(head.next, cache);
 };
+
+let n1 = new Node(1);
+n1.appendToTail(2);
+n1.appendToTail(3);
+let mid = n1.appendToTail('loop');
+n1.appendToTail(5);
+n1.appendToTail(6);
+let last = n1.appendToTail(7);
+last.next = mid
+
+console.log(loopDetection(n1).data);

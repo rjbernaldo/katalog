2d4863061e5ae423c5da5f61d4f95c351ae4222c
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-13 10:19:45 +0800

    Add solution for ex4

--- exercises/stacks-and-queues/ex4.js
+++ exercises/stacks-and-queues/ex4.js
@@ -4,6 +4,48 @@
 
 class MyQueue {
   constructor() {
-    
+    this.stack = [];
+    this.reversedStack = [];
+  }
+  
+  cloneReverse() {
+    if (this.reversedStack.length === 0) {
+      while (this.stack.length > 0) {
+        this.reversedStack.push(this.stack.pop());
+      }
+    }
+  }
+  
+  add(elem) {
+    this.stack.push(elem);
+  }
+  
+  remove() {
+    this.cloneReverse();
+    return this.reversedStack.pop();
+  }
+  
+  peek() {
+    this.cloneReverse();
+    return this.reversedStack[this.reversedStack.length - 1];
+  }
+  
+  isEmpty() {
+    return this.stack.length === 0;
   }
 }
+
+let queue = new MyQueue();
+queue.add(0);
+queue.add(1);
+queue.add(2);
+queue.add(3);
+console.log(queue.peek() === 0);
+queue.remove();
+queue.remove();
+queue.remove();
+queue.add(4);
+console.log(queue.peek() === 3);
+queue.remove();
+console.log(queue.peek() === 4);
+console.log(queue.isEmpty() === true)

4847fd83940ea69dffc33e2d9a0e4aa294ed5b0a
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-14 08:08:45 +0800

    Add solution for ex5

--- exercises/stacks-and-queues/ex5.js
+++ exercises/stacks-and-queues/ex5.js
@@ -4,3 +4,47 @@
 * into any other data structure (such as an array). The stack supports the
 * following operations: push, pop, peek, and isEmpty.
 */
+
+Array.prototype.peek = function () {
+  return this[this.length - 1];
+};
+
+Array.prototype.isEmpty = function () {
+  return this.length === 0;
+}
+
+const sortStack = (s) => {
+  let stack = [].concat(s);
+  let sortedStack = [];
+
+  while(stack.length > 0) {
+    let clonedStack = [].concat(stack);
+    let highest;
+    while(!clonedStack.isEmpty()) {
+      let current = clonedStack.pop();
+      if (!highest || current > highest) highest = current;
+    }
+
+    let cache = [];
+    let highestFound = false;
+    while(!highestFound) {
+      let current = stack.pop();
+      if (highest === current) {
+        sortedStack.push(current);
+        highestFound = true;
+        while(!cache.isEmpty()) {
+          stack.push(cache.pop());
+        }
+      } else {
+        cache.push(current);
+      }
+    }
+  }
+
+  return sortedStack;
+};
+
+let stack = [1,2,3,4,5];
+let sortedStack = sortStack(stack);
+console.log(stack);
+console.log(sortedStack);

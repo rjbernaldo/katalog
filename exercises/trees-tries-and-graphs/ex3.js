c873fb3d826d1703741c0fa902a5bca8d78ebbdb
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-16 13:34:52 +0800

    Add third exercise

--- exercises/trees-tries-and-graphs/ex3.js
+++ exercises/trees-tries-and-graphs/ex3.js
@@ -1,3 +1,5 @@
 /*
-*
+* Given a binary tree, design an algorithm which creates a linked list of all
+* the nodes at each depth (e.g., if you have a tree with depth D, you'll
+* have D linked lists)
 */

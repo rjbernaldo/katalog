1b06b6b99934a944c6d33dfd418420222954e61e
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-22 10:24:43 +0800

    Add rough and naive solution to ex1

--- exercises/trees-tries-and-graphs/ex1.js
+++ exercises/trees-tries-and-graphs/ex1.js
@@ -2,3 +2,37 @@
 * Given a directed graph, design an algorithm to find out whether there is a
 * route between two nodes.
 */
+
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+}
+
+const routeBetweenNodes = (n1, n2) => {
+  for (let i = 0; i < n1.children.length; i++) {
+    const child = n1.children[i];
+
+    if (child === n2) {
+      return true;
+    } else if (child.children.length > 0) {
+      const childResult = routeBetweenNodes(child, n2);
+      if (childResult) return childResult;
+    } else {
+      return false;
+    }
+  }
+
+  return false;
+};
+
+/* TESTS */
+const root = new Node('root');
+const child1 = new Node('child1');
+const child2 = new Node('child2');
+
+root.children.push(child1);
+child1.children.push(child2);
+
+console.log(routeBetweenNodes(root, child2), routeBetweenNodes(root, child2) === true);

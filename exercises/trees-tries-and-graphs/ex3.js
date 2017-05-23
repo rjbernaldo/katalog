1e7d2c1b02a8239e64d21492f7ef947b2042e67e
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-23 11:05:45 +0800

    Add pseudocode for ex3

--- exercises/trees-tries-and-graphs/ex3.js
+++ exercises/trees-tries-and-graphs/ex3.js
@@ -3,3 +3,18 @@
 * the nodes at each depth (e.g., if you have a tree with depth D, you'll
 * have D linked lists)
 */
+
+const listOfDepths = (node, depth = 1) => {
+  const set = [];
+
+  const currentDepth = [node.left, node.right];
+  set.push(currentDepth);
+
+  const nextDepth = []
+    .concat(listOfDepths(node.left))
+    .concat(listOfDepths(node.right));
+
+  set.push(nextDepth);
+
+  return set;
+};

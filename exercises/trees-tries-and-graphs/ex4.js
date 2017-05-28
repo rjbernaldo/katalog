42f8a6628980a35640aeaf7c3b87b845ebc81d24
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-29 07:58:57 +0800

    Add solution based on book

--- exercises/trees-tries-and-graphs/ex4.js
+++ exercises/trees-tries-and-graphs/ex4.js
@@ -3,3 +3,22 @@
 * of this question, a balanced tree is defined to be a tree such that the
 * heights of the two subtrees of any node never differ by more than one.
 */
+
+const checkBalanced = (node) => {
+  if (node === null) return true;
+
+  const heightDiff = getHeight(node.left) - getHeight(node.right);
+
+  if (Math.abs(heightDiff) > 1) return false;
+
+  return checkBalanced(node.left) && checkBalanced(node.right);
+};
+
+function getHeight(node) {
+  if (node === null) return -1;
+
+  return Math.max(
+    getHeight(node.left),
+    getHeight(node.right),
+  ) + 1;
+}

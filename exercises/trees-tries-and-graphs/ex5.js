8412492088c68300f7a9325eabefc893341925dd
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-30 15:45:38 +0800

    Add initial solution for ex5

--- exercises/trees-tries-and-graphs/ex5.js
+++ exercises/trees-tries-and-graphs/ex5.js
@@ -1,3 +1,20 @@
 /*
 * Implement a function to check if a binary tree is a binary search tree.
 */
+
+const validateBST = (node) => {
+  let leftNode = true;
+  let rightNode = true;
+
+  if (node.left !== null) {
+    if (node.left >= node.value) return false;
+    leftNode = validateBST(node.left);
+  }
+
+  if (node.right !== null) {
+    if (node.right <= node.value) return false;
+    rightNode = validateBST(node.right);
+  }
+
+  return leftNode && rightNode;
+};

5ce82b64e60747e9aadfd773c167da526b939378
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-31 08:13:31 +0800

    Add solution for ex6

--- exercises/trees-tries-and-graphs/ex6.js
+++ exercises/trees-tries-and-graphs/ex6.js
@@ -3,3 +3,13 @@
 * of a given node in a binary search tree. You may assume that each node has
 * a link to it's parent.
 */
+
+const successor = (query, node) => {
+  if (node === null) return false;
+
+  if (typeof node === 'undefined') node = query;
+  if (query.value < node.value) return node;
+  if (node.right === null) return successor(query, node.parent); // go up to parent
+
+  return successor(query, node.right); // go to right
+};

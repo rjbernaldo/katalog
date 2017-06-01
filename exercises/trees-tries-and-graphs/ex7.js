b4d2e97d789fb79d8cba9c938437e8bb4b8e4ae6
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-01 08:30:38 +0800

    Add naive and untested solution to ex7

--- exercises/trees-tries-and-graphs/ex7.js
+++ exercises/trees-tries-and-graphs/ex7.js
@@ -8,6 +8,47 @@
 
 const buildOrder = (projects, deps) => {
   // if projects list satisfies deps return projects
+  if (isSatisfied(projects, deps)) return projects;
   // else do some swapping
+  const swappedProjects = swapProjects(projects, deps);
   // recurse with new projects
+  return buildOrder(swappedProjects, deps);
 };
+
+function isSatisfied(projects, deps) {
+  for (let i = 0; i < deps.length; i++) {
+    const dep = deps[i];
+    const first = projects.indexOf(dep[0]);
+    const second = projects.indexOf(dep[1]);
+
+    if (first > second) return false;
+  }
+
+  return true;
+}
+
+function swapProjects(projects, deps) {
+  let swappedProjects = projects;
+
+  for (let i = 0; i < deps.length; i++) {
+    const dep = deps[i];
+    const first = swappedProjects.indexOf(dep[0]);
+    const second = swappedProjects.indexOf(dep[1]);
+
+    if (first > second) {
+      swappedProjects = [
+        ...projects.slice(0, second),
+        dep[0],
+        ...projects.slice(second + 1),
+      ];
+
+      swappedProjects = [
+        ...projects.slice(0, first),
+        dep[1],
+        ...projects.slice(first + 1),
+      ];
+    }
+  }
+
+  return swappedProjects;
+}

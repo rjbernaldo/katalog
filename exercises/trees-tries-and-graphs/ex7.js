a3c2cf32136f40dd66f6ad2854220c8a151fc36f
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-01 08:12:50 +0800

    Add initial pseudocode

--- exercises/trees-tries-and-graphs/ex7.js
+++ exercises/trees-tries-and-graphs/ex7.js
@@ -1,7 +1,13 @@
 /*
-* You are given a list of projects and a list of dependencies (which is a lit
+* You are given a list of projects and a list of dependencies (which is a list
 * of pairs of projects, where the second project is dependent on the first
 * project). All of a project's dependencies must be built before the project
 * is. Find a build order that will allow the projects to be built. If there is
 * no valid build order, return an error.
 */
+
+const buildOrder = (projects, deps) => {
+  // if projects list satisfies deps return projects
+  // else do some swapping
+  // recurse with new projects
+};

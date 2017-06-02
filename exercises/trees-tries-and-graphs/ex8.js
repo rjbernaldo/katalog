a19605d6f7d417da08a0d52b8780fa46f5e4c894
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-02 08:28:41 +0800

    Add initial solution

--- exercises/trees-tries-and-graphs/ex8.js
+++ exercises/trees-tries-and-graphs/ex8.js
@@ -3,3 +3,32 @@
 * nodes in a binary tree. Avoid storing additional nodes in a data structure.
 * NOTE: This is not necessarily a binary search tree.
 */
+
+const firstCommonAncestor = (n1, n2) => {
+  let sibling = getSibling(n1);
+  let parent = n1.parent;
+
+  while (!isFound(sibling, n2)) {
+    sibling = getSibling(parent);
+    parent = parent.parent;
+  }
+
+  return parent;
+};
+
+function getSibling(node) {
+  if (node === null || node.parent === null) return null;
+
+  const parent = node.parent;
+
+  return parent.left === node
+    ? parent.right
+    : parent.left;
+}
+
+function isFound(root, node) {
+  if (root === null) return false;
+  if (root === node) return true;
+
+  return isFound(root.left, node) || isFound(root.right, node);
+}

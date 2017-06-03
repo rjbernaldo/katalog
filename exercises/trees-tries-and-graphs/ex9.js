78fe13a7ebcd098ab07980a68b3d668a204d979e
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-03 13:37:11 +0800

    Fix typo

--- exercises/trees-tries-and-graphs/ex9.js
+++ exercises/trees-tries-and-graphs/ex9.js
@@ -1,6 +1,6 @@
 /*
 * A binary search tree was created by traversing through an array from left
 * to right and inserting each element. Given a binary search tree with
-* distinct elements, print all possible arrats that could have led to this
+* distinct elements, print all possible arrays that could have led to this
 * tree.
 */

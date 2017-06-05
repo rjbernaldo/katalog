af8987b203db38435137e2d5bfcb6672153c5cdc
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-06-05 13:12:38 +0800

    Add brute force solution

--- exercises/trees-tries-and-graphs/ex12.js
+++ exercises/trees-tries-and-graphs/ex12.js
@@ -5,3 +5,27 @@
 * or end at the root or a leaf, but it must go downwards (traveling only
 * from parent nodes to child nodes).
 */
+
+const pathsWithSum = (node, sum) => {
+  if (node === null) return 0;
+
+  const pathsFromNode = countPaths(node, sum, 0);
+
+  const pathsOnLeft = pathsWithSum(node.left, sum, 0);
+  const pathsOnRight = pathsWithSum(node.right, sum, 0);
+
+  return pathsFromNode + pathsOnLeft + pathsOnRight;
+};
+
+function countPaths(node, sum, currentSum) {
+  if (node === null) return 0;
+
+  const newSum = currentSum + node.value;
+
+  let total = 0;
+  if (newSum === sum) total += 1;
+  total += countPaths(node.left, sum, newSum);
+  total += countPaths(node.right, sum, newSum);
+
+  return total;
+}
